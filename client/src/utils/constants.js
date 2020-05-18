11b0dd0698f850747246c408b8757b8eda68efd2
Andronikus <tiagoafroque@gmail.com>
2020-05-18 21:11:19 +0100

    fix package.json for heroku deployment

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,5 @@
 const PORT = process.env.PORT || 5000;
+console.log("CONSTANTS:: ", process.env.PORT);
+
 export const BACKEND_URL = `http://localhost:${PORT}`;
 export const ENDPOINT = `localhost:${PORT}`;
